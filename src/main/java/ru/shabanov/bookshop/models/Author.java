1ec2e77d03dee79fc8157937322883090e2e07da
Ilya Shabanov <shabanoff.ilya@gmail.com>
2023-03-06 20:25:26 +0700

    Add author entity

---
+++ src/main/java/ru/shabanov/bookshop/models/Author.java
@@ -0,0 +1,64 @@
+package ru.shabanov.bookshop.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "author")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "photo")
+    private String photo;
+
+    @Column(name = "slug", nullable = false)
+    private String slug;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

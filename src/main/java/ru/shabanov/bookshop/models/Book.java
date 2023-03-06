470f5717e120539953ce4534056cb51142ae64f5
Ilya Shabanov <shabanoff.ilya@gmail.com>
2023-03-06 20:24:13 +0700

    Add book entity

---
+++ src/main/java/ru/shabanov/bookshop/models/Book.java
@@ -0,0 +1,109 @@
+package ru.shabanov.bookshop.models;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "book")
+public class Book {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "pub_date", nullable = false)
+    private LocalDate pubDate;
+
+    @Column(name = "is_bestseller", nullable = false)
+    private Boolean isBestseller;
+
+    @Column(name = "slug", nullable = false)
+    private String slug;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price", nullable = false)
+    private Integer price;
+
+    @Column(name = "discount", nullable = false)
+    private Integer discount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(LocalDate pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public Boolean getBestseller() {
+        return isBestseller;
+    }
+
+    public void setBestseller(Boolean bestseller) {
+        isBestseller = bestseller;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Integer discount) {
+        this.discount = discount;
+    }
+}
